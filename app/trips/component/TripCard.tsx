0a5c4b3c2fa26126e4fe4f9de198ec442ee98fdd
lucy <lucy@lucyui-MacBookPro.local>
2026-03-25 15:10:56 +0900

    feat: list style

---
+++ app/trips/component/TripCard.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+type Trip = {
+  id: string;
+  title: string;
+  country: string;
+  date: string;
+};
+
+type TripCardProps = {
+  trip: Trip;
+};
+
+export default function TripCard({ trip }: TripCardProps) {
+  return (
+    <Link href={`/trips/${trip.id}`} className="block">
+      <article className="
+        aspect-3/4
+        w-55
+        rounded-xl
+        bg-sky-50
+        p-4
+        shadow-lg
+        hover:-translate-y-1
+        transition
+        flex
+        items-end
+        ">
+        <div className="flex flex-col">
+            <h3 className="text-2xl black-han-sans">{trip.title}</h3>
+            <p className="mt-2 text-sm text-gray-600">{trip.country}</p>
+            <p className="mt-1 text-sm text-gray-500">{trip.date}</p>
+        </div>
+      </article>
+    </Link>
+  );
+}
--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,24 +1,21 @@
-import Link from "next/link";
+import TripCard from "./component/TripCard";
 
 const trips = [
-  { id: "1", title: "오사카 여행" },
-  { id: "2", title: "도쿄 여행" },
-  { id: "3", title: "제주도 여행" },
+  { id: "1", title: "오사카 여행", date: "2026.04.03 ~ 2026.04.07", country: "오사카" },
+  { id: "2", title: "도쿄 여행", date: "2026.04.03 ~ 2026.04.07", country: "도쿄" },
+  { id: "3", title: "제주도 여행", date: "2026.04.03 ~ 2026.04.07", country: "제주도" },
 ];
 
 export default function TripsPage() {
   return (
-    <main>
-      <h1>여행 리스트</h1>
-      <ul>
-        {
-          trips?.map((trip) => (
-            <li key={trip.id}>
-                <Link href={`/trips/${trip.id}`}>{trip.title}</Link>
-            </li>
-          ))
-        }
-      </ul>
+    <main className="p-5">
+      <h1 className="mb-6 text-2xl font-bold">여행 리스트</h1>
+
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(220px,220px))] gap-6 justify-start">
+        {trips.map((trip) => (
+          <TripCard key={trip.id} trip={trip} />
+        ))}
+      </div>
     </main>
-  )
+  );
 }
